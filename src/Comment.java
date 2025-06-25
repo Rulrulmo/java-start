155b18d48c998c4a62537ab2d08f2c0bf522e7ca
khmin <khmin@rsupport.com>
2025-06-25 18:29:33 +0900

    section 4 ~ 12

---
+++ src/Comment.java
@@ -0,0 +1,11 @@
+public class Comment {
+    public static void main(String[] args) {
+        System.out.println("Hello Java1"); // Hello Java1을 출력합니다.
+//        System.out.println("Hello Java2"); 한줄 주석
+
+        /*
+        System.out.println("Hello Java3");
+        System.out.println("Hello Java4");
+         */
+    }
+}
